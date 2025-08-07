73127502aa2b0cef0a5e8e7f12468b6ccd1286a7
Gideon <gideonkimaiyo254@gmail.com>
2025-08-07 14:00:02 +0300

    Add print_fibonacci function

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length <= 0:
+        print([])
+        return
+
+    fib_sequence = [0]
+
+    if length > 1:
+        fib_sequence.append(1)
+
+    while len(fib_sequence) < length:
+        next_number = fib_sequence[-1] + fib_sequence[-2]
+        fib_sequence.append(next_number)
+
+    print(fib_sequence)
+if __name__ == "__main__":
+    print_fibonacci(4)
